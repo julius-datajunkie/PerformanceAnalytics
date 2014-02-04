Added Geltner Return calculation and fixed return tests

PerformanceAnalytics/returns.py
@@ -3,6 +3,16 @@ import numpy as np
 import math
 import utils
 
+def annualized(R, scale=None, geometric=True):
+    n = len(R)
+    result = 0
+    if scale is None:
+        scale = utils.periodicity(R).scale
+    if geometric:
+        result = math.pow((1 + R).prod(), scale / n) - 1
+    else:
+        result = R.mean() * scale
+    return result
 
 def cumulative(R, geometric=True):
     """
@@ -16,7 +26,17 @@ def cumulative(R, geometric=True):
     else:
         return (1 + R).prod() - 1
 
-
+def calculate(price, method="discrete"):
+    """
+    
+    """
+    if method == "simple" or method == "discrete":
+        ret = price / price.shift(1) - 1
+    elif method == "compound" or method == "log":
+        ret = np.log(price).diff()
+        
+    return ret
+              
 def excess(R, Rf=0):
     """
     Calculates the returns of an asset in excess of the given "risk free rate"
@@ -26,15 +46,39 @@ def excess(R, Rf=0):
     """
     return R - Rf
 
+def centered(R):
+    """
+    Calculate the demeaned returns
+    """
+    return R - R.mean()
 
-def Geltner(R):
+def geltner(R):
     """
     Calculates returns by removing estimating or liquidity bias in
     real estate index returns. It has since been applied with success
     to other return series that show autocorrelation or illiquidity.
     """
-    pass
+    f_acf = R.apply(utils.acf).iloc[1].values
+    return (R - R.shift(1) * f_acf) / (1 - f_acf) 
 
+def relative(Ra, Rb):
+    """
+    Calculate the ratio of the cumulative performance for two assets 
+    through time.
+    """
+    try:
+        benchmark = "benchmark"
+        if Rb.name != None: #Rb is a time series
+            benchmark = Rb.name
+        else:
+            Rb.name = benchmark # Timeseries has to have a name in order for join to work
+        combined = Ra.join(Rb)
+    except AttributeError:
+        combined = Ra.join(Rb,rsuffix="bm")
+    
+    result = (combined.ix[:, :-1] + 1).cumprod() / (combined.ix[:, -1] + 1).cumprod()
+    result.columns = ["{}/{}".format(col, benchmark) for col in result.columns]
+    return result
 
 def rebalancing(R, weights):
     """
@@ -61,14 +105,3 @@ def rebalancing(R, weights):
     portfolio_return.index.name = 'Date'
     return portfolio_return
 
-
-def annualized(R, scale=None, geometric=True):
-    n = len(R)
-    result = 0
-    if scale is None:
-        scale = utils.periodicity(R).scale
-    if geometric:
-        result = math.pow((1 + R).prod(), scale / n) - 1
-    else:
-        result = R.mean() * scale
-    return result

tests/test_returns.py
@@ -1,9 +1,48 @@
 from PerformanceAnalytics import returns
+from pandas.util.testing import assert_frame_equal, assert_almost_equal
 import pandas as pd
-
+import numpy as np
 
 class TestReturns:
     def test_annualized(self):
         dates = pd.date_range("1/1/2010", periods=12, freq="M")
         df = pd.DataFrame([0.1 for i in range(12)], index=dates)
-        assert abs(returns.annualized(df) - 2.138428) < 0.0001
+        assert_almost_equal(returns.annualized(df), 2.138428)
+        
+    def test_cumulative(self):
+        pass
+    
+    def test_calculate(self):
+        dates = pd.date_range("1/1/2010", periods=12, freq="M")
+        prices = pd.DataFrame([i for i in range(1, 13)], index=dates)
+        rets = returns.calculate(prices)
+        ans = pd.DataFrame([float(i+1)/i - 1 for i in range(1, 13)], index=dates).shift(1)
+        assert_frame_equal(rets, ans)
+        
+        rets = returns.calculate(prices, "log")
+        ans = pd.DataFrame([np.nan, 0.69314718, 0.40546511, 0.28768207, 0.22314355, 
+                           0.18232156, 0.15415068, 0.13353139, 0.11778304, 
+                           0.10536052, 0.09531018, 0.08701138], index=dates)
+        assert_frame_equal(rets, ans)
+        
+    def test_geltner(self):
+        dates = pd.date_range("1/1/2010", periods=5, freq="M")
+        df = pd.DataFrame([i for i in range(5)], index=dates)
+        df = returns.geltner(df)
+        ans = pd.DataFrame([np.nan, 1.666667, 2.666667, 3.666667, 4.666667], index=dates)
+        assert_frame_equal(df, ans)
+        
+    def test_relative(self):
+        dates = pd.date_range("1/1/2010", periods=5, freq="M")
+        Ra = pd.DataFrame([0.1*i for i in range(1, 6)], index=dates)
+        Rb = pd.DataFrame([0.1 for i in range(1, 6)], index=dates)
+        relative_rets = returns.relative(Ra, Rb)
+        ans = pd.DataFrame([1.000000, 1.090909, 1.289256, 1.640872, 2.237552], index=dates,
+                           columns = ["0/benchmark"])
+        assert_frame_equal(relative_rets, ans)
+        
+        #Test for when Rb is a time series
+        Rb = pd.TimeSeries([0.1 for i in range(1, 6)], index=dates)
+        relative_rets = returns.relative(Ra, Rb)
+        assert_frame_equal(relative_rets, ans)        
+    